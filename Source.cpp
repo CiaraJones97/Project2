da4dce4c14be3a543f113626fb648267a11634eb
Ciara <ciarajones071@gmail.com>
2018-07-19 15:46:12 -0500

    Project 2 - finished

--- Source.cpp
+++ Source.cpp
@@ -11,29 +11,32 @@ using namespace std;
 
 int main()
 {
+	//Vector for making a full and complete Binary Tree
 	vector<string> nodestuff{ "XX", "XX", "XX", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL", "XX", "XX", "XX","NULL", "NULL", "XX", "NULL", "NULL", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL", "XX","XX", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL", "XX", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL", "XX", "XX", "NULL", "NULL", "XX", "NULL", "NULL" };
 	map<char, string> myTree;
 	string myString;
 
 	Binary_Tree<string> Tree; //Tree is an object of Binary_Tree
 	
+	//Input the text file 
 	ifstream infile;
 	infile.open("morse.txt");
 
+	//Take the data from the text file and put it into a map
 	while (infile)
 	{
 		infile >> myString;
 		char key = myString[0];
-		/*cout << key << endl;*/
 		string value = myString.substr(1, -1);
-		/*cout << value << endl;*/
 		myTree[key] = value;
 	}
 	
+	//Put the information from the map into the Binary Tree
 	Tree.insert(myTree, nodestuff);
-	//Tree.read_tree(myTree);
-	cout <<"Decoded equals: "<< Tree.Decode(".... ._ ._.. ._.. . ._.. .._ .___ ._ ....") << endl;
-	cout <<"Encoded equals: " << Tree.Encode("do you know da wae?", myTree)<< endl;
+
+	//Output the decoded and encoded messages
+	cout <<"Decoded equals: "<< Tree.Decode("_.. ___ _.__ ___ .._ _._ _. ___ .__ _.. ._ .__ ._ .") << endl;
+	cout <<"Encoded equals: " << Tree.Encode("Hello", myTree)<< endl;
 	
 	infile.close();
 	system("pause");
